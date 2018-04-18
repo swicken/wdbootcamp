18b8b568cf8c76dd14af2053b30059ec52f21618
swicken <scott.wicken@n8id.com>
2018-04-18 07:46:55 -0400

    Learning stuffs

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -1,67 +1,80 @@
+
+
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var mongoose = require ("mongoose");
 var app = express();
 
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
-  var campgrounds = [{
-      name: "Salmon Creek",
-      image: "https://farm8.staticflickr.com/7042/7121867321_65b5f46ef1.jpg"
-      
-  }, {
-      name: "Running River",
-      image: "https://pixabay.com/get/eb32b9072ef3063ed1584d05fb1d4e97e07ee3d21cac104497f3c67aa6edb4b9_340.jpg"
-  }, {
-      name: "Granite Hill",
-      image: "https://farm4.staticflickr.com/3455/3753652218_266bca0b93.jpg"
-  },{
-      name: "Salmon Creek",
-      image: "https://farm8.staticflickr.com/7042/7121867321_65b5f46ef1.jpg"
-      
-  }, {
-      name: "Running River",
-      image: "https://pixabay.com/get/eb32b9072ef3063ed1584d05fb1d4e97e07ee3d21cac104497f3c67aa6edb4b9_340.jpg"
-  }, {
-      name: "Granite Hill",
-      image: "https://farm4.staticflickr.com/3455/3753652218_266bca0b93.jpg"
-  },{
-      name: "Salmon Creek",
-      image: "https://farm8.staticflickr.com/7042/7121867321_65b5f46ef1.jpg"
-      
-  }, {
-      name: "Running River",
-      image: "https://pixabay.com/get/eb32b9072ef3063ed1584d05fb1d4e97e07ee3d21cac104497f3c67aa6edb4b9_340.jpg"
-  }, {
-      name: "Granite Hill",
-      image: "https://farm4.staticflickr.com/3455/3753652218_266bca0b93.jpg"
-  }]
 
+//Schema
+
+var campgroundSchema = new mongoose.Schema( {
+    name: String,
+    image: String,
+    description: String
+});
+
+var campground = mongoose.model("Campground", campgroundSchema);
+// campground.create({
+//     name: "Granite Hill",
+//     image: "https://farm6.staticflickr.com/5319/7407436246_0ac54dd559.jpg",
+//     description: "Some place by a pond or a lake or something. I dont know, look at the picture d"
+// })
 app.get("/", (req,res) => {
     res.render("landing");
 })
 
 app.get("/campgrounds", (req,res) => {
 
+  let camps = campground.find({}, (err,allCampgrounds)=> {
+      if (err) {
+          console.log(err);
+      } else {
+        res.render("index", { campgrounds: allCampgrounds});
+      }
+  });  
   
-  res.render("campgrounds", { campgrounds: campgrounds});
 });
+
+
 
 app.post("/campgrounds", (req,res) => {
    let name = req.body.name;
    let image = req.body.image;
-   let newCampground = {name:name, image:image};
-   campgrounds.push(newCampground);
-   res.redirect("/campgrounds")
+   let desc = req.body.description;
+   let newCampground = {name:name, image:image, description: desc};
+   campground.create(newCampground, (err, campground)=>{
+       if(err) {
+           console.log(err);
+       } else {
+           console.log("Created: ");
+           console.log(campground);
+           res.redirect("/campgrounds");
+       }
+   });
+   
 });
 
 app.get("/campgrounds/new", (req,res)=> {
    res.render("new"); 
 });
 
+app.get("/campgrounds/:id", function (req,res){
+    campground.findById(req.params.id, (err, foundCampground)=>{
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("show", {campground : foundCampground});
+        }
+    })
+});
+
 app.listen(3000, function() {
-    console.log("YelpCamp server is listening on port " + process.env.PORT);
+    console.log("YelpCamp server is listening on port " + 3000);
 })
